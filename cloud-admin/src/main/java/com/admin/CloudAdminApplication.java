65e7c4ab91c4d30517663381b6f48292045e164b
kaze <957786753@qq.com>
2017-10-29 22:24:28 +0800

    kaze

---
+++ cloud-admin/src/main/java/com/admin/CloudAdminApplication.java
@@ -0,0 +1,20 @@
+package com.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author kaze 2017/10/29
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class CloudAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAdminApplication.class, args);
+	}
+
+}
