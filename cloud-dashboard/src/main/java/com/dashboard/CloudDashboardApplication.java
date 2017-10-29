08fee6b5433b2c86feb4c7d141b08cc559dbb57d
kaze <957786753@qq.com>
2017-10-29 16:53:44 +0800

    kaze

--- cloud-dashboard/src/main/java/com/dashboard/CloudDashboardApplication.java
+++ cloud-dashboard/src/main/java/com/dashboard/CloudDashboardApplication.java
@@ -2,6 +2,7 @@ package com.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class CloudDashboardApplication {
 
