ee14bf5904bc47f4cb59718072fdbc60bfa9abf5
kaze <957786753@qq.com>
2017-10-29 14:36:36 +0800

    kaze

--- cloud-api/src/main/java/com/api/swagger2/Swagger2Config.java
+++ cloud-api/src/main/java/com/api/swagger2/Swagger2Config.java
@@ -22,7 +22,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.web.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.api.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
