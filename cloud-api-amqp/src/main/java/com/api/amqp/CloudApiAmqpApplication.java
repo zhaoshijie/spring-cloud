0c72ba9f624c17202b76bc3e0b6f047d6f9f8998
kaze <957786753@qq.com>
2017-10-29 17:48:43 +0800

    kaze

--- cloud-api-amqp/src/main/java/com/api/amqp/CloudApiAmqpApplication.java
+++ cloud-api-amqp/src/main/java/com/api/amqp/CloudApiAmqpApplication.java
@@ -2,13 +2,17 @@ package com.api.amqp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author kaze 2017/10/29
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CloudApiAmqpApplication {
 
 	public static void main(String[] args) {
