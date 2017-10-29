6bd5734d5254f983a71f88fd9e6e7a35cf47a6fd
kaze <957786753@qq.com>
2017-10-29 13:02:19 +0800

    kaze

--- cloud-zuul/src/main/java/com/zuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/zuul/CloudZuulApplication.java
@@ -1,7 +1,5 @@
 package com.zuul;
 
-import com.netflix.zuul.FilterProcessor;
-import com.zuul.filter.MyFilterProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -14,7 +12,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class CloudZuulApplication {
 
 	public static void main(String[] args) {
-		FilterProcessor.setProcessor(new MyFilterProcessor());
 		SpringApplication.run(CloudZuulApplication.class, args);
 	}
 
