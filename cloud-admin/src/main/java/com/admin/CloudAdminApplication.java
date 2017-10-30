2c84e10643fc58088487003e84cd3b4f4ab5d368
kaze <957786753@qq.com>
2017-10-30 23:25:11 +0800

    kaze

--- cloud-admin/src/main/java/com/admin/CloudAdminApplication.java
+++ cloud-admin/src/main/java/com/admin/CloudAdminApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author kaze 2017/10/29
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableAdminServer
+@EnableTurbine
 public class CloudAdminApplication {
 
 	public static void main(String[] args) {
